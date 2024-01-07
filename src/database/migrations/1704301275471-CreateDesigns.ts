b7d7ba3dbc135d88b67da2deacbcccae8ee7372b
Héctor <hsoriano9986@gmail.com>
2024-01-07 18:07:03 +0100

    creacion de estructura base, instalacion de scripts y migraciones

---
+++ src/database/migrations/1704301275471-CreateDesigns.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateDesigns1704301275471 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
