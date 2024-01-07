cec67c6d3cc71e0d77526160337dd47340ffd738
Héctor <hsoriano9986@gmail.com>
2024-01-07 20:23:40 +0100

    migracion de ultima entidad, clientes

---
+++ src/database/migrations/1704655362199-CreateClientss.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateClientss1704655362199 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
