6f26ae0d3366ac23fc9ed1bbd805c6b6be6f1224
Héctor <hsoriano9986@gmail.com>
2024-01-09 17:20:03 +0100

    entidad clients incorporada como migracion

--- src/database/migrations/1704655469975-CreateClients.ts
+++ src/database/migrations/1704655469975-CreateClients.ts
@@ -1,11 +1,55 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateClients1704655469975 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "clients",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "user_id",
+            type: "int",
+            isUnique: true,
+          },
+          {
+            name: "first_name",
+            type: "varchar",
+            length: "50",
+          },
+          {
+            name: "last_name",
+            type: "varchar",
+            length: "50",
+            isNullable: true,
+          },
+          {
+            name: "phone_number",
+            type: "varchar",
+            length: "40",
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ["user_id"],
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+          },
+        ],
+      }),
+      true
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+    await queryRunner.dropTable("clients");
+  }
 }
