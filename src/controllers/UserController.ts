8d48ae2fb34f071b52bf392e4ab26cc6d150b701
Héctor <hsoriano9986@gmail.com>
2024-01-14 12:16:52 +0100

    repository de user integrado en las peticiones existentes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,14 @@
 import { Controller } from "./Controller";
 import { Request, Response } from "express";
 import { User } from "../models/User";
+import { AppDataSource } from "../database/data-source";
 
 export class UserController implements Controller {
 
     async getAll(req: Request, res: Response): Promise<void> {
         try {
-            const allUsers = await User.find();
+            const userRepository = AppDataSource.getRepository(User);
+            const allUsers = await userRepository.find();
             res.status(200).json(allUsers);
         } catch (error) {
             res.status(500).json({
@@ -18,7 +20,9 @@ export class UserController implements Controller {
     async getById(req: Request, res: Response): Promise<void> {
         try {
             const id = +req.params.id;
-            const user = await User.findOneBy({
+
+            const userRepository = AppDataSource.getRepository(User);
+            const user = await userRepository.findOneBy({
                 id: id,
             }) 
             res.status(200).json(user);
