e380aaac519ebd7c14f8e1967c1ad8977ed8fad1
Héctor <hsoriano9986@gmail.com>
2024-02-14 17:52:31 +0100

    descomentada la opción de borrar usuarios por id para realización de pruebas en la db

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 const userController = new UserController();
 
 router.get("/:id", auth, userController.getById);//ver NUESTRO perfil
-//router.delete("/:id", userController.delete);
+router.delete("/:id", userController.delete);
 //router.get("/",auth, userController.getAll);
 
 
